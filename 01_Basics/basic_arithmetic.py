7c24da74e563ed9a1a2328e542c588f70da821ca
aniket pawar <aniketpawarpatil2020@gmail.com>
2025-11-29 10:34:40 +0530

    perform arithmatic operation

---
+++ 01_Basics/basic_arithmetic.py
@@ -0,0 +1,10 @@
+a=int(input("enter first number:\n"))
+b=int(input("enter second number:\n"))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor Division:",a//b)
+print("Modulus:",a%b)
+print("Exponentiation:",a**b)
+
