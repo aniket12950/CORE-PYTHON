7dc42bf7a5674acef7f25ddef75433129bc8f318
Aniket Pawar <aniketpawarpatil2020@gmail.com>
2025-11-29 10:45:40 +0530

    define a variable and use type() function

--- 01_Basics/variable.py
+++ 01_Basics/variable.py
@@ -1,11 +1,14 @@
-
-name="Aniket"
+name = "Aniket"
 print(type(name))
-age=17
+age = 17
 print(type(age))
-percentage=87.5
+percentage = 87.5
 print(type(percentage))
-is_student=True
+is_student = True
 print(type(is_student))
-Grade='A'
+Grade = 'A'
 print(type(Grade))
+pi_value = 3.14159
+print(type(pi_value))
+complex_number = 2 + 3j
+print(type(complex_number))
