fe914577cdb6308361c76ded776ccd76b3e7c419
aniket pawar <aniketpawarpatil2020@gmail.com>
2025-11-30 21:14:15 +0530

    perform indexing operation on string

---
+++ 03_String/string_indexing.py
@@ -0,0 +1,11 @@
+str="aniket pawar"
+print("original string:",str)
+print("First character of string:", str[0])
+print("Last character of string:", str[-1])
+print("Character at index 3:", str[3])
+print("Substring from index 0 to 4:", str[0:5])
+print("Substring from index 3 to end:", str[3:])
+print("Substring from start to index 5:", str[:6])
+print("Substring with step 2 from index 0 to 10:", str[0:11:2]) #str[start:end:step]
+print("Reversed string using slicing:", str[::-1])
+
