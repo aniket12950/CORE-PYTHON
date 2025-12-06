6ed037d0ae2cfa065631587d330eb5d585f8f792
aniket pawar <aniketpawarpatil2020@gmail.com>
2025-12-06 22:14:03 +0530

    create a student class

---
+++ 10_OOP/class.py
@@ -0,0 +1,11 @@
+class Student:
+    
+    # Display function
+    def display(self):
+        print("Hello, I am a Student object.")
+
+# Create object of class
+s1 = Student()
+
+# Call display function using object
+s1.display()
