e67ea0da94926b6bbf8aa3403be496b2f615a708
aniket pawar <aniketpawarpatil2020@gmail.com>
2026-02-02 18:43:45 +0530

    simple login system programs

---
+++ 08_Dictionary/simple_login_system.py
@@ -0,0 +1,11 @@
+users = {
+    "admin": "admin123",
+    "user": "admin1234"
+}
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username in users and users[username] == password: 
+    print("Login successful!")
+else:
+    print("Invalid username or password.")
